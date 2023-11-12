1b8e72a0b69d691552154bf315d1abed0f1495ae
jjesvin21 <jjesvin21jose@gmail.com>
2023-11-12 16:16:40 +0530

    small change in week 1 and 2

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import NavWeb from "./NavWeb";
+import NavMobile from "./NavMobile";
+export default function NavBar() {
+  const isWideScreen = window.innerWidth > 600;
+
+  return <div>{isWideScreen ? <NavWeb /> : <NavMobile />}</div>;
+}
--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,20 +1,10 @@
 import React from "react";
-import NavWeb from "../Components/NavBar/NavWeb";
-import NavMobile from "../Components/NavBar/NavMobile";
-import { Route, Routes } from "react-router-dom";
-import page1 from "../Components/page1";
-import page2 from "../Components/page2";
-
-export default function Home() {
-  const isWideScreen = window.innerWidth > 600;
-
+import NavBar from "../Components/NavBar/NavBar";
+export default function home() {
   return (
     <div>
-      {isWideScreen ? <NavWeb /> : <NavMobile />}
-      <Routes>
-        <Route path="/home/page1" Component={page1}></Route>
-        <Route path="home/page2" Component={page2}></Route>
-      </Routes>
+      <NavBar />
+      <div>Home Page</div>
     </div>
   );
 }

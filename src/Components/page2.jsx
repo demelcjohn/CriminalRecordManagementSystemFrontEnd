de4921fd49585b58b15128e42248316383e4775f
jjesvin21 <jjesvin21jose@gmail.com>
2023-11-15 13:11:51 +0530

    week 3 Jesvin profile page

--- src/Components/page2.jsx
+++ src/Components/page2.jsx
@@ -7,3 +7,73 @@ export default function page2() {
     </div>
   );
 }
+
+{
+  /* <table className="profile_page_table">
+            <tr>
+              <td colSpan={2} className="profile_page_table_col_img">
+                <img src={dp} />
+              </td>
+            </tr>
+            <tr>
+              <td className="profile_page_table_col">
+                <b>UID</b>
+              </td>
+              <td className="profile_page_table_col">1111 1111 1111 1111</td>
+            </tr>
+            <tr>
+              <td className="profile_page_table_col">
+                <b>Name</b>
+              </td>
+              <td className="profile_page_table_col">Jesvin Jose</td>
+            </tr>
+            <tr>
+              <td className="profile_page_table_col">
+                <b>Gender</b>
+              </td>
+              <td className="profile_page_table_col">Male</td>
+            </tr>
+            <tr>
+              <td className="profile_page_table_col">
+                <b>DOB</b>
+              </td>
+              <td className="profile_page_table_col">21/11/2001</td>
+            </tr>
+            <tr>
+              <td className="profile_page_table_col">
+                <b>House Name</b>
+              </td>
+              <td className="profile_page_table_col">House</td>
+            </tr>
+            <tr>
+              <td className="profile_page_table_col">
+                <b>Street</b>
+              </td>
+              <td className="profile_page_table_col">Street 1</td>
+            </tr>
+            <tr>
+              <td className="profile_page_table_col">
+                <b>City</b>
+              </td>
+              <td className="profile_page_table_col">City 1</td>
+            </tr>
+            <tr>
+              <td className="profile_page_table_col">
+                <b>Pin</b>
+              </td>
+              <td className="profile_page_table_col">111111</td>
+            </tr>
+            <tr>
+              <td className="profile_page_table_col">
+                <b>State</b>
+              </td>
+              <td className="profile_page_table_col">State 1</td>
+            </tr>
+            <tr>
+              <td className="profile_page_table_col">
+                <b>Country</b>
+              </td>
+              <td className="profile_page_table_col">Country 1</td>
+            </tr>
+          </table> */
+}
--- src/Pages/profile_page.jsx
+++ src/Pages/profile_page.jsx
@@ -1,80 +1,188 @@
 import NavBar from "../Components/NavBar/NavBar";
 import "./profile_page.css";
 import dp from "../img/dp.png";
-
-import React from "react";
+import { TextField } from "@mui/material";
+import { styled } from "@mui/system"; // Change this line
+import React, { useState } from "react";
 
 export default function Profile_page() {
+  const [user, setuser] = useState({
+    name: "jesvin",
+    house_name: "house name",
+    street: "street1",
+    city: "city 1",
+    state: "state 1",
+    country: "country 1",
+    pin: "111 111",
+    dob: "21-11-2001",
+    gender: "Male",
+    uid: "1111 1111 1111 1111",
+  });
+
+  const StyledTextFieldLong = styled(TextField)({
+    color: "black",
+    width: "90vw",
+    "@media (min-width:600px)": {
+      width: "50vw",
+    },
+    "& .MuiInputBase-root.Mui-disabled": {
+      color: "black", // change input text color
+      "-webkit-text-fill-color": "black", // for webkit browsers
+    },
+    "& .MuiFormLabel-root.Mui-disabled": {
+      color: "black", // change label color
+    },
+    "& .MuiInputBase-input.Mui-disabled": {
+      color: "black", // change value color
+      "-webkit-text-fill-color": "black", // for webkit browsers
+    },
+  });
+
+  const StyledTextFieldShort = styled(TextField)({
+    color: "black",
+    width: "42.5vw",
+    "@media (min-width:600px)": {
+      width: "20vw",
+    },
+
+    "& .MuiInputBase-root.Mui-disabled": {
+      color: "black", // change input text color
+      "-webkit-text-fill-color": "black", // for webkit browsers
+    },
+    "& .MuiFormLabel-root.Mui-disabled": {
+      color: "black", // change label color
+    },
+    "& .MuiInputBase-input.Mui-disabled": {
+      color: "black", // change value color
+      "-webkit-text-fill-color": "black", // for webkit browsers
+    },
+  });
+
   return (
     <div>
       <NavBar></NavBar>
       <div className="profile_page_outerdiv">
         <div className="profile_page_innerdiv">
-          <table className="profile_page_table">
+          <table>
             <tr>
               <td colSpan={2} className="profile_page_table_col_img">
                 <img src={dp} />
               </td>
             </tr>
             <tr>
-              <td className="profile_page_table_col">
-                <b>UID</b>
+              <td colSpan={2} className="profile_page_table_col">
+                <StyledTextFieldLong
+                  label="Name"
+                  variant="standard"
+                  value={user.name}
+                  focused
+                  style={{ color: "black" }}
+                  disabled
+                />
               </td>
-              <td className="profile_page_table_col">1111 1111 1111 1111</td>
+            </tr>
+            <tr>
+              <td colSpan={2} className="profile_page_table_col">
+                <StyledTextFieldLong
+                  label="UID"
+                  variant="standard"
+                  value={user.uid}
+                  focused
+                  style={{ color: "black" }}
+                  disabled
+                />
+              </td>
             </tr>
             <tr>
               <td className="profile_page_table_col">
-                <b>Name</b>
+                <StyledTextFieldShort
+                  label="Gender"
+                  variant="standard"
+                  value={user.gender}
+                  focused
+                  style={{ color: "black" }}
+                  disabled
+                />
               </td>
-              <td className="profile_page_table_col">Jesvin Jose</td>
+              <td className="profile_page_table_col">
+                <StyledTextFieldShort
+                  label="DOB"
+                  variant="standard"
+                  value={user.dob}
+                  focused
+                  style={{ color: "black" }}
+                  disabled
+                />
+              </td>
+            </tr>
+            <tr>
+              <td colSpan={2} className="profile_page_table_col">
+                <StyledTextFieldLong
+                  label="House Name"
+                  variant="standard"
+                  value={user.house_name}
+                  focused
+                  style={{ color: "black" }}
+                  disabled
+                />
+              </td>
             </tr>
             <tr>
               <td className="profile_page_table_col">
-                <b>Gender</b>
+                <StyledTextFieldShort
+                  label="Street"
+                  variant="standard"
+                  value={user.street}
+                  focused
+                  style={{ color: "black" }}
+                  disabled
+                />
               </td>
-              <td className="profile_page_table_col">Male</td>
+              <td className="profile_page_table_col">
+                <StyledTextFieldShort
+                  label="City"
+                  variant="standard"
+                  value={user.city}
+                  focused
+                  style={{ color: "black" }}
+                  disabled
+                />
+              </td>
             </tr>
             <tr>
               <td className="profile_page_table_col">
-                <b>DOB</b>
+                <StyledTextFieldShort
+                  label="State"
+                  variant="standard"
+                  value={user.state}
+                  focused
+                  style={{ color: "black" }}
+                  disabled
+                />
               </td>
-              <td className="profile_page_table_col">21/11/2001</td>
+              <td className="profile_page_table_col">
+                <StyledTextFieldShort
+                  label="PIN"
+                  variant="standard"
+                  value={user.pin}
+                  focused
+                  style={{ color: "black" }}
+                  disabled
+                />
+              </td>
             </tr>
             <tr>
               <td className="profile_page_table_col">
-                <b>House Name</b>
+                <StyledTextFieldShort
+                  label="Country"
+                  variant="standard"
+                  value={user.gender}
+                  focused
+                  style={{ color: "black" }}
+                  disabled
+                />
               </td>
-              <td className="profile_page_table_col">House</td>
-            </tr>
-            <tr>
-              <td className="profile_page_table_col">
-                <b>Street</b>
-              </td>
-              <td className="profile_page_table_col">Street 1</td>
-            </tr>
-            <tr>
-              <td className="profile_page_table_col">
-                <b>City</b>
-              </td>
-              <td className="profile_page_table_col">City 1</td>
-            </tr>
-            <tr>
-              <td className="profile_page_table_col">
-                <b>Pin</b>
-              </td>
-              <td className="profile_page_table_col">111111</td>
-            </tr>
-            <tr>
-              <td className="profile_page_table_col">
-                <b>State</b>
-              </td>
-              <td className="profile_page_table_col">State 1</td>
-            </tr>
-            <tr>
-              <td className="profile_page_table_col">
-                <b>Country</b>
-              </td>
-              <td className="profile_page_table_col">Country 1</td>
+              <td></td>
             </tr>
           </table>
         </div>
